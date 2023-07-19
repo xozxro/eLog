8d1a3c99a7a2442f8a5fd4db688c32c105c49da9
Michael Benjamin <91173681+xozxro@users.noreply.github.com>
2023-07-19 14:04:09 -0400

    Update data.py

--- eLog/data.py
+++ eLog/data.py
@@ -3,6 +3,7 @@ import os
 def get_token():
     ENV_TOKEN=False
     try: ENV_TOKEN = os.getenv('NOTION_TOKEN') 
+    except: pass
     if not ENV_TOKEN: print(f"[!] eLog | Token not found. Have you ran configure.py?")
     return ENV_TOKEN
 
